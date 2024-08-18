c4591bf2e722865c190f22c6e8a3db11cb388d51
Liz Avila <871714+Liz4v@users.noreply.github.com>
2024-08-18 14:16:38 -0400

    cleaned up some unused js

--- js/footer.js
+++ js/footer.js
@@ -1,14 +1,3 @@
-// pasted from repeated scripts in the footer of the html files
-
-$(document).ready(function(){
-    $(window).scroll(function() { // check if scroll event happened
-        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-            
-        } else {
-            
-        }
-    });
-});
 
 jQuery(function() {
     jQuery('.links_topo').click(function() {
@@ -33,32 +22,7 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == document.getElementById("myModal") || event.target == document.getElementById("close")) {
-        $("#myModal").css("display","none");
-    }
-    if (event.target == document.getElementById("myModal2") || event.target == document.getElementById("close2")) {
-        $("#myModal2").css("display","none");
-    }
-    if (event.target == document.getElementById("myModal3") || event.target == document.getElementById("close3")) {
-        $("#myModal3").css("display","none");
-    }
-}  
-
 $('.carousel').carousel({
     pause: true,
     interval: false
 });
-
-
-function changeEn() {
-    window.location.href = window.location.href.split(/[?#]/)[0] + '?idioma=en';
-}
-
-function changePt() {
-    window.location.href = window.location.href.split(/[?#]/)[0] + '?idioma=pt';
-}
-
-$('.phone_us').mask('(00) 00000-0000');   // pt variant
-$('.phone_us').mask('0 (000) 000-0000');  // en variant
